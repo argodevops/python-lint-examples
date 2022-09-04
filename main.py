6d1712ad15043dd0a5c781ddf078429a87a75457
Chris <CodeBuild@AWS.com>
2022-09-04 17:02:06 +0100

    chore: add linting and formatting

---
+++ main.py
@@ -0,0 +1,24 @@
+import requests
+import time
+
+newHeaders = {"Content-type": "application/json", "Accept": "text/plain"}
+
+# fmt: off
+heatmap_gp = bytes([
+    0, 255, 255, 255, # White
+    64, 255, 255, 0,  # Yellow
+    128, 255, 0, 0,   # Red
+    255, 0, 0, 0])    # Black
+# fmt: on
+
+response = requests.post(
+    "https://httpbin.org/post", data={"id": 1, "name": "Jessa"}, headers=newHeaders
+)
+
+print("Status code: ", response.status_code)
+
+response_Json = response.json()
+print("Printing Post JSON data")
+print(response_Json["data"])
+
+print("Content-Type is ", response_Json["headers"]["Content-Type"])
